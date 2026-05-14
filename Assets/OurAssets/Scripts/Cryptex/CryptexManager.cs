4cdf8cbb6b4029689a940d8f5c16a4bc47491a92
DavidBarker05 <davidbarker9611@gmail.com>
2026-05-14 20:49:49 +0200

    rotate door and disable interaction on cryptex success

--- Assets/OurAssets/Scripts/Cryptex/CryptexManager.cs
+++ Assets/OurAssets/Scripts/Cryptex/CryptexManager.cs
@@ -9,14 +9,20 @@ public class CryptexManager : MonoBehaviour
     Player m_Player;
     [SerializeField]
     FirstPersonCharacter m_FirstPersonCharacter;
+    [SerializeField]
+    GameObject m_Door;
+    [SerializeField]
+    Vector3 m_DoorRotation = new Vector3(0f, -90f, 0f);
+    [SerializeField]
+    CryptexInteraction m_CryptexInteraction;
 
-	void Awake()
-	{
+    void Awake()
+    {
         if (Instance && Instance != this) Destroy(gameObject);
         else Instance = this;
-	}
+    }
 
-	public void CheckNameMatches(CrypexRing[] cryptexRings)
+    public void CheckNameMatches(CrypexRing[] cryptexRings)
     {
         if (cryptexRings == null || string.IsNullOrWhiteSpace(SpiritNameManager.Instance.SpiritName)) return;
         if (cryptexRings.Length != SpiritNameManager.NAME_LENGTH)
@@ -28,7 +34,8 @@ public class CryptexManager : MonoBehaviour
         for (int i = 0; i < SpiritNameManager.NAME_LENGTH; ++i) name.Append(char.ToUpper(cryptexRings[i].Letter));
         if (name.ToString().Equals(SpiritNameManager.Instance.SpiritName, System.StringComparison.OrdinalIgnoreCase))
         {
-            Debug.Log("You Win");
+            m_Door.transform.Rotate(m_DoorRotation, Space.Self);
+            m_CryptexInteraction.gameObject.SetActive(false);
             MinigameManager.Instance.OnMinigameBeaten(Minigames.Cryptex);
             m_Player.ChangeCharacter(m_FirstPersonCharacter);
         }
