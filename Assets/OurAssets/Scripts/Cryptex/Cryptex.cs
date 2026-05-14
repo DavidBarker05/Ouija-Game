fe5aa41cc26ac112594058f0cedaf21f1cae3bd4
DavidBarker05 <davidbarker9611@gmail.com>
2026-05-14 13:32:44 +0200

    correctly loop back around for cryptex

--- Assets/OurAssets/Scripts/Cryptex/Cryptex.cs
+++ Assets/OurAssets/Scripts/Cryptex/Cryptex.cs
@@ -38,18 +38,20 @@ public class Cryptex : MonoBehaviour
     {
         if (m_CryptexRings == null || index >= m_CryptexRings.Length) throw new System.ArgumentOutOfRangeException("Index out of range");
         int rot = rotation == RingRotation.PrevLetter ? -1 : (rotation == RingRotation.NextLetter ? 1 : 0);
-		float angle = LETTER_ROTATION * rot;
+        float angle = LETTER_ROTATION * rot;
         Vector3 eulerRotation = m_RingUpRotationAxis switch
         {
-			RingNextLetterRotation.PosX => new Vector3(angle, 0f, 0f),
-			RingNextLetterRotation.NegX => new Vector3(-angle, 0f, 0f),
-			RingNextLetterRotation.PosY => new Vector3(0f, angle, 0f),
-			RingNextLetterRotation.NegY => new Vector3(0f, -angle, 0f),
-			RingNextLetterRotation.PosZ => new Vector3(0f, 0f, angle),
-			RingNextLetterRotation.NegZ => new Vector3(0f, 0f, -angle),
-			_ => throw new System.NotSupportedException($"{m_RingUpRotationAxis} is not supported")
+            RingNextLetterRotation.PosX => new Vector3(angle, 0f, 0f),
+            RingNextLetterRotation.NegX => new Vector3(-angle, 0f, 0f),
+            RingNextLetterRotation.PosY => new Vector3(0f, angle, 0f),
+            RingNextLetterRotation.NegY => new Vector3(0f, -angle, 0f),
+            RingNextLetterRotation.PosZ => new Vector3(0f, 0f, angle),
+            RingNextLetterRotation.NegZ => new Vector3(0f, 0f, -angle),
+            _ => throw new System.NotSupportedException($"{m_RingUpRotationAxis} is not supported")
         };
         m_CryptexRings[index].Transform.localEulerAngles += eulerRotation;
-        m_CryptexRings[index].Letter = (char)Mathf.Clamp(char.ToUpper(m_CryptexRings[index].Letter) + rot, 'A', 'Z');
-	}
+        m_CryptexRings[index].Letter = (char)(char.ToUpper(m_CryptexRings[index].Letter) + rot);
+        if (m_CryptexRings[index].Letter < 'A') m_CryptexRings[index].Letter = 'Z';
+        else if (m_CryptexRings[index].Letter > 'Z') m_CryptexRings[index].Letter = 'A';
+    }
 }
